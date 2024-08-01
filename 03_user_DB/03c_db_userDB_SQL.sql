5b0869ff67aba924abab69248edc532fe9f6d928
Sara <Sarahakimi206@yahoo.com>
2024-08-01 12:02:45 +0200

    SHA1 Algorithmus &  Änderung &  Update in der datei vom Tabelle

--- 03_user_DB/03c_db_userDB_SQL.sql
+++ 03_user_DB/03c_db_userDB_SQL.sql
@@ -1,4 +1,4 @@
-/* USER TAB. Version 2 */
+/* USER TAB. Version 3 */
 
 /* Table users */
 DROP TABLE IF EXISTS boo.users;
@@ -16,16 +16,37 @@ CREATE TABLE IF NOT EXISTS boo.users
 DESCRIBE boo.users;
 
 /* Daten */
--- Nachteil: PWDs liegen offen! 
-INSERT INTO boo.users (userName, familyName, firstName, userpwd) 
-VALUES ('amir_1990', 'Hosseini', 'Amir', '1234'  );
+-- Besser: PWDs verschlüsselt mit SHA1! 
+INSERT INTO boo.users (userName, familyName, firstName, userPwd) 
+VALUES ('johndoe123', 'Doe', 'John', SHA1('1234'));
 
-INSERT INTO boo.users (userName, familyName, firstName, userpwd) 
-VALUES ('maryam_sh85', 'Shahidi', 'Maryam', 'S356d#');
+INSERT INTO boo.users (userName, familyName, firstName, userPwd) 
+VALUES ('emilybrown22', 'Brown', 'Emily', SHA1('7#Dx'));
 
-INSERT INTO boo.users (userName, familyName, firstName, userpwd) 
-VALUES ('reza_tehrani', 'Tehrani', 'Reza', 'sdfazh');
-
+INSERT INTO boo.users (userName, familyName, firstName, userPwd) 
+VALUES ('michaelsmith7', 'Smith', 'Michael',SHA1('user1234'));
 
 /* Inhalte : Ergebnistabelle */
 SELECT * FROM boo.users;
+
+
+/* Änderungen an bestehender Tabelle / Struktur*/
+
+ALTER TABLE
+    boo.users
+ADD
+    userPLZ VARCHAR(5) NOT NULL DEFAULT "00000"
+;
+
+/* Struktur +  Inhalt*/
+DESCRIBE boo.users;
+SELECT * FROM boo.users;
+
+
+/* Update vo Daten */
+UPDATE boo.users SET userPLZ = "70367" WHERE id= 1;
+UPDATE boo.users SET userPLZ = "70321" WHERE firstName= 'Emily';
+UPDATE boo.users SET userPLZ = "81371" WHERE familyName= 'Smith';
+
+SELECT * FROM boo.users;
+
